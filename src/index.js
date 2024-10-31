4b97d36f3dca242678cdc7147a8bead6390d7544
Tacako <iberarmandosg@gmail.com>
2024-10-31 14:45:25 -0600

    API "Calificaciones"

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const morgan = require("morgan");
+const database = require("./database");
+
+//configuración inicial
+const app = express();
+const PORT = 4000; // Cambiar el puerto
+const IP = '192.168.56.1';
+
+app.listen(PORT, IP, () => {
+    console.log(`Servidor en ejecución en http://${IP}${PORT}`);
+});
+
+//Middlewares
+app.use(morgan("dev"))
+
+//Rutas
+app.get('/calificaciones', async (req, res) => {
+    const connection = await database.getConnection();
+    const result = await connection.query("SELECT * FROM calificaciones");
+    console.log(result)
+});
+
+
